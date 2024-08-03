96b1f12fd6a2bec375c13b226994f88018e1f213
Kirill-Ul <kirill.ulyanov.00@mail.ru>
2024-08-03 17:44:31 +0300

    Add files via upload

---
+++ Dz6 Ulyanov K.A..py
@@ -0,0 +1,55 @@
+
+# задача 1
+# # # дан  список
+# # запрещено использовать sort, max, map преобразовывать список в другие типы тоже нельзя
+# # найти минимальный элемент в списке
+
+# list_of_nums = [3, 1, 5, 6, 9]
+# min_num = list_of_nums[0]
+# for num in list_of_nums:
+#     if num < min_num:
+#         min_num = num
+# print(min_num)
+#
+
+# задача 2
+# # дан  список
+# # запрещено использовать sort, max, count, sum, map , преобразовывать список в другие типы тоже нельзя
+# # найти произведение всех элементов в этом списке
+
+# list_of_nums = [3, 1, 5, 4]
+# x = 1
+# for num in list_of_nums:
+#     x *= num
+# print(x)
+
+# задача 3
+# # дан список
+# запрещено использовать sort, max, count, sum, map
+# # удалить из этого списка, все отрицательные элементы
+
+# my_list = [1, -2, 3, 5, -6, 7]
+# i = 0
+# while i < len(my_list):
+#     if my_list[i] < 0:
+#         del my_list[i]
+#         i -= 1
+#     i += 1
+# print(f"Список без отрицательных элементов: {my_list}")
+
+# задача 4
+# # дан  список
+# запрещено использовать sort, max, count, sum, map
+# # сформировать новый список, с положительными числами
+
+# list_of_nums = [3, 1, -8, 5, -4]
+# positive_numbers = [] # создаем пустой список
+# for num in list_of_nums:
+#     if num > 0:
+#         positive_numbers.append(num) # вкладываем в пустой список отобранные числа
+# print(positive_numbers)
+
+
+
+
+
