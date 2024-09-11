c408f33f5f36414009acb5ca6b192e8b6b9fbba5
Kirill-Ul <kirill.ulyanov.00@mail.ru>
2024-09-11 21:31:47 +0300

    Add files via upload

---
+++ Dz 13.py
@@ -0,0 +1,31 @@
+# 1.	Напишите функцию подсчета периметра прямоугольника с двумя аргументами width и height. Функция должна возвращать значение.
+#
+# def perimetr(width, height):
+#     a = (width + height) * 2
+#     return a
+# print(perimetr(3,4))
+
+# 2.	Напишите функцию возведения числа в степень с двумя аргументами number и degree. Функция должна возвращать значение.
+# def results(number, degree):
+#     x = number ** degree
+#     return x
+# print(results(3,4))
+
+# 3.	Напишите функцию, подсчитывающую количество слов в введенном тексте с одним аргументом text. Функция должна вернуть количество слов.
+
+# def quantity(text):
+#     number_of_words = len(text.split())
+#     return number_of_words
+# print(quantity('Сегодня хорошая погода '))
+
+# 4.	*Напишите функцию, которая принимает бесконечное количество чисел и возвращает среднеарифметическое, переданных чисел. Функция должна вернуть среднеарифметическое, переданных чисел.
+
+
+# def numbers(*args):
+#     amount = 0
+#     for i in args:
+#        amount = amount + i
+#     arithmetic_mean = amount / len(args)
+#     return arithmetic_mean
+# print(numbers(4, 2, 6, 4))
+
